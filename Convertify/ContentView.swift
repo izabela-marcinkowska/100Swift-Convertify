3fa1c27105e5d43d841fdfd44f691d532df86272
Devy <izabela.marcinkowska@outlook.com>
2024-08-23 13:24:47 +0200

    feat: Temperature input field

--- Convertify/ContentView.swift
+++ Convertify/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var temperatureInput = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Temperature", value: $temperatureInput, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+            }
         }
-        .padding()
     }
 }
 
