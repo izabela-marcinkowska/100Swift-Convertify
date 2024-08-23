ace780cf20ffaf6200297bb5be26c0c99f7cf4e3
Devy <izabela.marcinkowska@outlook.com>
2024-08-23 12:18:32 +0200

    Initial Commit

---
+++ Convertify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Convertify
+//
+//  Created by Izabela Marcinkowska on 2024-08-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Convertify/ConvertifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertifyApp.swift
+//  Convertify
+//
+//  Created by Izabela Marcinkowska on 2024-08-23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
